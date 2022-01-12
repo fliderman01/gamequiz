d4a448c346d34ec7ee8d951a1ef663eb496e5a26
fliderman01 <fliderman01@gmail.com>
2022-01-12 15:38:39 +0400

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DarkMode from './components/DarkMode';
+import data from './data.json';
 import './style.css';
 
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [pages, setPages] = useState(0);
+
+  // import questins from data.json
+  const questions = data.variants;
+  // when finished change variants to questions here and in json
+
+  // starts Quiz
+  const startQuiz = () => {
+    setPages(1);
+  };
+  // Play again
+  const playAgain = () => {
+    setPages(0);
+  };
+  // show results at the end of the questions
+  // currentQuestion > questions.length && setPages(2);
+  if (currentQuestion > questions.length) {
+    setCurrentQuestion(0);
+    setPages(2);
+  }
+
+  // answer option
+  const nextQuestion = (correct) => {
+    correct && setScore(score + 1);
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  // render start Quiz, Questions and Results
+  const renderQuiz = () => {
+    if (pages == 0) {
+      return (
+        <div className="startQuiz bg">
+          <h2>General Knowledge Quiz</h2>
+          <button className="StartBtn" onClick={startQuiz}>
+            Start Quiz?
+          </button>
+        </div>
+      );
+    } else if (pages > 0) {
+      return (
+        <div className="questions bg">
+          <div className="questionText">
+            <h3>
+              Question {currentQuestion + 1} out of {questions.length}:
+            </h3>
+            <p>{questions[currentQuestion].questionTxt}</p>
+          </div>
+          <div className="answers">
+            {questions[currentQuestion].answerOpts.map((answerOption) => (
+              <button
+                key={Math.floor(Math.random() * 9999)}
+                onClick={() => nextQuestion(answerOption.isCorrect)}
+              >
+                {answerOption.answerTxt}
+              </button>
+            ))}
+          </div>
+        </div>
+      );
+    } else if (pages > 1) {
+      return (
+        <div className="results bg">
+          <h2>
+            You're Score is {score} out of {questions.length}
+          </h2>
+          <button className="playAgainBtn" onClick={playAgain}>
+            Play again
+          </button>
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
       <DarkMode />
-
-      <div className="startQuiz bg">
-        <h2>General Knowledge Quiz</h2>
-        <button className='StartBtn'>Start Quiz?</button>
-      </div>
-
-      <div className="questions bg">
-        <div className="questionText">
-          <h3>Question 1 out of 14:</h3>
-          <p>Who is the Queen of England?</p>
-        </div>
-        <div className="answers">
-          <button>Elisabeth II</button>
-          <button>Meeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeee</button>
-          <button>Antony Hopkins</button>
-          <button>Jennifer Lawrence of arabia</button>
-        </div>
-      </div>
-
-      <div className="results bg">
-        <h2>You're Score is 10 out of 14</h2>
-        <button className="playAgainBtn">Play again</button>
-      </div>
+      {renderQuiz()}
     </div>
   );
 }
-// if display == 0 show startQuiz else if display == 1 show questions else show results;
-// display initial = 0; startQuiz onClick = setDisplay = 1; if nextQuestion == questions.length eetDisplay = 2
